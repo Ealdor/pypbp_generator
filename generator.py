52e00fcdc36afdc1deb61acfc73730dfab0da93b
ealdor <ealdorj@gmail.com>
2015-12-26 12:10:15 +0100

    Code refactor and bug fix.

--- generator.py
+++ generator.py
@@ -57,7 +57,7 @@ class Position:
         self.number = number
         self.adjacents = []
         self.ini = False
-        self.pair = None
+        self.pair = self
 
     def __repr__(self):
         return "%s[%s]" % (repr(self.coordinate), self.number)
@@ -92,7 +92,7 @@ class Puzzle:
         inicializa la lista de posiciones candidatas con el resto.
 
         """
-        print('inicializando puzzle')
+        print('inicializando puzzle (candidatos', len(self.candidate), ')')
         for pos1 in self.initial:
             for pos2 in self.initial:  # añadimos las Posiciones adyacentes a las Posiciones del Puzzle.
                 if pos2.coordinate in [(pos1.coordinate[0] + 1, pos1.coordinate[1]),
@@ -110,6 +110,7 @@ class Puzzle:
                         break
             else:
                 self.final.append(pos1)
+        print('='*40)
 
 
 class Generator:
@@ -211,6 +212,8 @@ class Checker:
             rama (TreeNode): rama actual.
             root (Position): posicion desde la que se comienza a generar el arbol auxiliar.
 
+        TODO: Mejorar la velocidad pasandole una lista con las nuevas incorporaciones y si no le afecta ni mirarlo,
+
         """
         if self.finish:
             return
@@ -237,7 +240,6 @@ class Checker:
                             aux.append(a.name)
                         if adj not in aux:  # para que no vuelva sobre si mismo.
                             self.three_check(adj, rama.add_child(name=adj), root)
-
         if dist == self.number and father.number == self.number and father is not root.pair and\
                 abs(int(round(math.sqrt((father.pair.coordinate[0] - root.pair.coordinate[0])**2 +
                                         (father.pair.coordinate[1] - root.pair.coordinate[1])**2)))) <= root.number:
@@ -251,6 +253,7 @@ class Checker:
                     w.number = 1
                     w.ini = False
                     w.way = []
+                    w.pair = w
                     self.puzzle.candidate.append(w)
                 self.finish = True
 
@@ -287,6 +290,7 @@ class Checker:
                 w.number = 1
                 w.ini = False
                 w.way = []
+                w.pair = w
                 self.puzzle.candidate.append(w)
             self.finish = True
 
@@ -384,7 +388,10 @@ def check(puzz, chec):
         chec (Checker): Comprobador a usar.
 
     """
+    aux = 0
     for pos1 in puzz.final:
+        aux += 1
+        print('progreso:', aux, 'de', len(puzz.final), ' '*40, end='\r')
         if pos1.number == chec.number and pos1.ini:
             # print('generando arbol')
             chec.three_check(pos1, chec.t.add_child(name=pos1), pos1)
@@ -399,23 +406,30 @@ def found_error(i):
             i (int): numero analizado.
 
     """
-    print('numero de errores: ', len(p.candidate)/i)
+    print('\nnumero de errores:', int(len(p.candidate)/i))
     for pos1 in p.final:  # volver a construir la lista de candidatos.
         if pos1.number == 1 and pos1 not in p.candidate:
             for pos_ad in pos1.adjacents:
                 if pos_ad.number == 1:
-                    p.candidate.append(pos1)
-                    p.final.remove(pos1)
-                    break
+                    if pos_ad not in p.candidate:
+                        p.candidate.append(pos_ad)
+                    if pos_ad in p.final:
+                        p.final.remove(pos_ad)
+                    if pos1 not in p.candidate:
+                        p.candidate.append(pos1)
+                    if pos1 in p.final:
+                        p.final.remove(pos1)
         elif pos1.number < i and len(pos1.way) < i:
             for w in pos1.way:
                 w.number = 1
                 w.ini = False
                 w.way = []
+                w.pair = w
                 p.candidate.append(w)
                 p.final.remove(w)
     [p.final.remove(pos1) for pos1 in p.candidate if pos1 in p.final]
     print('finales: ', len(p.final), ' / ', 'candidatos: ', len(p.candidate))
+    print('='*40)
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
@@ -427,14 +441,17 @@ if __name__ == '__main__':
     it1 = it = int(sys.argv[3])  # numero de iteraciones por numero.
     while it2 > 1:
         while it > 0:
-            print('numero: ', it2, ' / iteracion: ', it)
+            print('numero:', it2, '- iteracion:', it)
             g = Generator(p, it2)  # creamos el generador.
             generate(p, g)  # generamos el puzzle.
             for it3 in range(it2, itm + 1):  # buscar errores por cada uno de los numeros entre el maximo y el generado.
-                print('buscando errores del numero: ', it3)
+                print('buscando errores del numero:', it3)
                 c = Checker(p, it3)  # creamos el comprobador.
                 check(p, c)  # comprobamos.
             found_error(it2)
+            if len(p.candidate) == 0:
+                it2 = 0
+                break
             it -= 1
         it = it1
         it2 -= 1
